0530ca5599a0aebefcd1f41cb1850511dc784ea2
Austin  Burke <aburk3@gmail.com>
2018-09-30 00:01:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,8 @@ def line(number_in_line)
   if number_in_line.size === 0 
     puts "The line is currently empty."
   else
+    number_in_line.each_with_index do |value, index|
+      
     puts "The line is currently: "
   end
 end
