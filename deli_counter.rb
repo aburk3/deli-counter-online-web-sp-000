8f37f545170866bfac087e4976a5f1abb143ae8c
Austin  Burke <aburk3@gmail.com>
2018-09-30 00:12:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,11 @@
 def line(number_in_line)
-  number_in_line.each_with_index do |value, index|
-    place_in_line = index + 1 
-    statement = "The line is currently: "
-    statement << "#{place_in_line}. #{value}."
+  if number_in_line.size === 0 
+    puts "The line is empty."
+  else
+    number_in_line.each_with_index do |value, index|
+      place_in_line = index + 1 
+      statement = "The line is currently: "
+      statement << "#{place_in_line}. #{value}."
   end
 end
 
