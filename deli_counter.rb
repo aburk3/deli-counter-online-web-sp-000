cda3757115c018fa55b9cbdf30e4b9fd1ca170bd
Austin  Burke <aburk3@gmail.com>
2018-09-30 00:18:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(number_in_line)
   if number_in_line.size === 0 
-    "The line is empty."
+    "The line is currently empty."
   else
     number_in_line.each_with_index do |value, index|
       place_in_line = index + 1 
