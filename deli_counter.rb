84708408481889fd34bf2679a99f2aa57ab3b338
Austin  Burke <aburk3@gmail.com>
2018-09-30 00:28:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(number_in_line)
     statement = "The line is currently: "
     number_in_line.each_with_index do |value, index|
       place_in_line = index + 1 
-      statement << "#{place_in_line}. #{value}."
+      statement << "#{place_in_line}. #{value}. "
     end
     puts statement
   end
