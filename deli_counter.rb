272f8b4d2e2cca5abecb390b340cd7cf03a31430
Austin  Burke <aburk3@gmail.com>
2018-09-30 00:24:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(number_in_line)
       place_in_line = index + 1 
       statement = "The line is currently: "
       statement << "#{place_in_line}. #{value}."
+      puts statement
     end
   end
 end
