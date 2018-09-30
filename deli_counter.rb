33e1d6c60d396899a752050384f6881b51210e8e
Austin  Burke <aburk3@gmail.com>
2018-09-30 00:26:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(number_in_line)
   if number_in_line.length == 0 
     puts "The line is currently empty."
   else
+    statement = "The line is currently: "
     number_in_line.each_with_index do |value, index|
       place_in_line = index + 1 
-      statement = "The line is currently: "
       statement << "#{place_in_line}. #{value}."
       puts statement
     end
